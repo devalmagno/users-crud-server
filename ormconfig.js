c86277279ba5d7d448ea162c6606c4fb8636a31f
devalmagno <devalmagno@gmail.com>
2021-03-29 21:38:41 -0300

    orm config vars

--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,11 @@
 console.log(`Database URL ${process.env.DATABASE_URL}`);
 module.exports = {
     "type": "postgres",
-    "url": process.env.DATABASE_URL,
+    "host": process.env.HOST,
+    "port": 5432,
+    "username": process.env.USER,
+    "password": process.env.PASS,
+    "database": process.env.DB,
     "entities": [
         "dist/entities/**/*.js"
     ],
