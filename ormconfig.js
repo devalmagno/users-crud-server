abd8bf3793d05a2d9cc0c73c7b7f6ce05b524500
devalmagno <devalmagno@gmail.com>
2021-03-29 21:45:49 -0300

    trying to deploy

--- ormconfig.js
+++ ormconfig.js
@@ -2,7 +2,6 @@ console.log(`Database URL ${process.env.DATABASE_URL}`);
 module.exports = {
     "type": "postgres",
     "url": process.env.DATABASE_URL,
-    "host":
     "entities": [
         "dist/entities/**/*.js"
     ],
