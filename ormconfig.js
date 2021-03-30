57d32edd76e4cd5f34447ec178ca0bcb27e1e5d5
devalmagno <devalmagno@gmail.com>
2021-03-29 21:43:55 -0300

    trying heroku

--- ormconfig.js
+++ ormconfig.js
@@ -1,11 +1,8 @@
 console.log(`Database URL ${process.env.DATABASE_URL}`);
 module.exports = {
     "type": "postgres",
-    "host": process.env.HOST,
-    "port": 5432,
-    "username": process.env.USER,
-    "password": process.env.PASS,
-    "database": process.env.DB,
+    "url": process.env.DATABASE_URL,
+    "host":
     "entities": [
         "dist/entities/**/*.js"
     ],
